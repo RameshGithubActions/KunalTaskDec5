5694a336f36438201725d98cd0ecff910801df22
rameshkpit <ramesh.p@kpit.com>
2023-12-06 16:44:08 +0530

    Initial Code

--- add_build.py
+++ add_build.py
@@ -2,7 +2,7 @@ import json
 
 with open('package.json', 'r+') as f:
     data = json.load(f)
-    data['scripts']['build'] = 'webpack --config webpack.config.js'
+    data['scripts']['build'] = 'npm install && npm test'
     print(data['scripts'])
     f.seek(0)
     json.dump(data, f, indent=4)

eed6bd70c0be157439e3f230eb0141253fbe0a62
rameshkpit <ramesh.p@kpit.com>
2023-12-06 16:54:43 +0530

    Added simple build and test string for checking purpose only

--- add_build.py
+++ add_build.py
@@ -2,7 +2,8 @@ import json
 
 with open('package.json', 'r+') as f:
     data = json.load(f)
-    data['scripts']['build'] = 'npm install && npm test'
+    data['scripts']['build'] = "echo 'Build not configured'"
+    data['scripts']['test'] = "echo 'Test not configured'"
     print(data['scripts'])
     f.seek(0)
     json.dump(data, f, indent=4)

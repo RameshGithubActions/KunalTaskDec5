733b8e35f01d4548b5202292a05caa132d0fe628
rameshkpit <ramesh.p@kpit.com>
2023-12-18 14:37:35 +0530

    System arguments....

---
+++ check.py
@@ -0,0 +1,9 @@
+import sys
+
+if __name__ = "__main__":
+    if len(sys.argv) > 1:
+        variable_from_argument = sys.argv[1]
+        print(f"Variable from argument: {variable_from_argument}")
+        
+    else:
+        print("No arguments provided!!!!")

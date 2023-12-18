143e17c1c5db6220077c0158e7d63962a6ba64a4
rameshkpit <ramesh.p@kpit.com>
2023-12-18 14:38:50 +0530

    Syntax error

--- check.py
+++ check.py
@@ -1,6 +1,6 @@
 import sys
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     if len(sys.argv) > 1:
         variable_from_argument = sys.argv[1]
         print(f"Variable from argument: {variable_from_argument}")
